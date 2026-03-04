7ba419639cf87dad3b5ebbde44edb80f4d2165fd
Hapatronik <daniels.mjatlins@gmail.com>
2026-03-04 14:30:42 +0200

    uzrakstīta ievade

--- cementa_kalkulators.py
+++ cementa_kalkulators.py
@@ -1,5 +1,13 @@
 def ievade():
-
+    cementa_daudzums=float(input("Ievadiet cementa daudzumu"))
+    if cementa_daudzums<0:
+        print("cementa daudzumam jābūt pozitīvam, aprēķini ir nepareizi")
+    smilsu_cena=float(input("Ievadiet smilšu cenu"))
+    if smilsu_cena<0:
+        print("smilšu cenai jābūt pozitīvai, aprēķini ir nepareizi")
+    cementa_cena=float(input("Ievadiet cementa cenu"))
+    if cementa_cena<0:
+        print("cementa cenai jābūt pozitīvai, aprēķini ir nepareizi")
 
 def aprekini(cementa_daudzums, smilsu_cena, cementa_cena):
 
